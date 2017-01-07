9e8b51512804c33d24ca1cd3e90659d54028d204
candy <90candy.com@gmail.com>
2017-01-07 14:05:57 +0800

    设置标签控制器 工具条上的图片渲染

--- 项目框架 - by Candy/项目框架 - by Candy/框架部分/Common(共用部分)/(Main)主框架/MainTabBarController.m
+++ 项目框架 - by Candy/项目框架 - by Candy/框架部分/Common(共用部分)/(Main)主框架/MainTabBarController.m
@@ -22,18 +22,23 @@
     [super viewDidLoad];
     
     NSArray *viewControllers = @[[[HomeViewController alloc]init],[[SchoolViewController alloc]init],[[MessageViewController alloc]init],[[MineViewController alloc]init]];
-    
+    //标签控制器 - 标题
     NSArray *tabBarItemTitle = @[@"首 页",@"学 校",@"消 息",@"我 的"];
-    
-    NSArray *tabBarItemImage = @[[UIImage imageNamed:@"首页"],[UIImage imageNamed:@"首页"],[UIImage imageNamed:@"首页"],[UIImage imageNamed:@"首页"]];
-    
-    NSArray *tabBarItemSelectedImage = @[[UIImage imageNamed:@"首页-select"],[UIImage imageNamed:@"首页-select"],[UIImage imageNamed:@"首页-select"],[UIImage imageNamed:@"首页-select"]];
-    
+    //标签控制器 - 正常图片
+    NSArray *tabBarItemImageName = @[@"menu_home_normal", @"menu_school_normal", @"menu_interactive_normal", @"menu_mine_normal"];
+    //标签控制器 - 选中图片
+    NSArray *tabBarItemSelectedImageName = @[@"menu_home_selected", @"menu_school_selected", @"menu_interactive_selected", @"menu_mine_selected"];
     _VCArray = [NSMutableArray array];
     
     for (int i = 0; i<viewControllers.count; i++) {
         UIViewController * vc = viewControllers[i];
-        UITabBarItem * item = [[UITabBarItem alloc] initWithTitle:tabBarItemTitle[i] image:tabBarItemImage[i] selectedImage:tabBarItemSelectedImage[i]];
+        //方法一：直接初始化
+        //UITabBarItem * item = [[UITabBarItem alloc] initWithTitle:tabBarItemTitle[i] image:[UIImage imageNamed:tabBarItemImageName[i]] selectedImage:[UIImage imageNamed:tabBarItemSelectedImageName[i]]];
+        //方法二：设置图片渲染
+        UITabBarItem * item = [[UITabBarItem alloc] init];
+        [item setTitle:tabBarItemTitle[i]];
+        item.image = [[UIImage imageNamed:tabBarItemImageName[i]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        item.selectedImage = [[UIImage imageNamed:tabBarItemSelectedImageName[i]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
         vc.tabBarItem = item;
         vc.title = tabBarItemTitle[i];
         UINavigationController * nav = [[UINavigationController alloc] initWithRootViewController:vc];
